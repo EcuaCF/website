c47d9c8d95b8fdbef0e0a778390d3afd154f701d
EcuaCF <jcabrera@ecuacf.com>
2025-10-15 13:57:44 -0500

    improve config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // You likely already have this if you're using GitHub Pages
-  basePath: "/website", // 👈 this tells Next.js where it lives
-  assetPrefix: "/website/", // 👈 ensures static assets load correctly
-};
-module.exports = nextConfig
-
-export default nextConfig;
+  basePath: "/website",
+  assetPrefix: "/website/",
+}
+module.exports = nextConfig;

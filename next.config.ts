4719d5a202c04ecdf6f02ff89357edc60e5b0c59
EcuaCF <jcabrera@ecuacf.com>
2025-10-15 14:00:12 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/website',
+        permanent: true, // Use false for temporary redirects
+      },
+    ];
+  },
   basePath: "/website",
   assetPrefix: "/website/",
 }
